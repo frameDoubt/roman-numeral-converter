a36ef2ccf99502c1745a47e73c7cdbe2f2613411
Brandon Wright <brandon.w.wright4@gmail.com>
2023-06-21 14:53:37 -0700

    add thousand denomination calculation

--- convertToRoman.js
+++ convertToRoman.js
@@ -1,4 +1,10 @@
 function convertToRoman(num) {
+  const denominations = [1000, 500, 100, 50, 10, 5, 1];
+  const romanNumeral = '';
+  // needs to be divisible by 1000 
+  if (num / 1000 > 0) {
+    let numN =  Math.floor(num / 1000);
+  }
   return "XXXVII";
  }
 
